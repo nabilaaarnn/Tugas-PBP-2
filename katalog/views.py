dc9962344aa4efceaa0dc077e825c604b7a1180b
Your Name <rizka.nisrina@ui.ac.id>
2022-09-12 20:44:46 +0700

    tugas 22

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_catalog(request):
+    data_catalog = CatalogItem().objects.all()
+    data_catalog = {
+        'list_katalog': data_catalog,
+        'nama': 'Nabila',
+        'npm' : '2106653344'
+    }
+
+    return render(request, "katalog.html", context)
+
