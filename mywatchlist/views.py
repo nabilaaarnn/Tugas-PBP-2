beb542c652561f62bbd48b443239bbbd22801b45
Your Name <rizka.nisrina@ui.ac.id>
2022-09-21 17:11:37 +0700

    Tugas 03 - II

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -10,6 +10,21 @@ def show_mywatchlist(request):
         'nama': 'Nabila',
         'npm' : '2106653344'
     }
-    print(data_mywatchlist)
-    return render(request, "mywatchlist.html", context)
+    return render(request, "Mywatchlist.html", context)
+
+def show_xml(request):
+    data = mywatchlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = mywatchlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request,id):
+    data = mywatchlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data = mywatchlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
