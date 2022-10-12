0aaf556c8896aae2883caf9abe0f2c5dfd7069b7
Your Name <rizka.nisrina@ui.ac.id>
2022-10-12 21:59:46 +0700

    Tugas 6

--- todolist/views.py
+++ todolist/views.py
@@ -70,7 +70,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            response = HttpResponseRedirect(reverse("todolist:show_todolist"))
+            response = HttpResponseRedirect(reverse("todolist:show_todolist_ajax"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
         else:
